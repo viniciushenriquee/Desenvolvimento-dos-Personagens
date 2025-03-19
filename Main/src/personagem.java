1723462b4c078ae36a1e4342f55ffc597d7bd811
Vinícius Henrique <111530961+viniciushenriquee@users.noreply.github.com>
2025-03-19 23:56:51 +0000

    Personagem

---
+++ Main/src/personagem.java
@@ -0,0 +1,13 @@
+public class personagem {
+ 
+ protected string nome;
+ protected int forca;
+ protected int vida;
+
+ public Personagem(String nome,int vida, int  forca){
+    this.nome = nome;
+    this.hp = hp;
+    this.forca = forca;
+ }
+
+}
